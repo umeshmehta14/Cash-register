28fafd4558ec5edc731ed1b5d903c2a4c2a91dd3
umeshmehta14 <ishaanmehta782@gmail.com>
2022-09-22 13:04:36 +0530

    finally

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const noOfnotes = document.querySelectorAll(".no-of-note");
 
 const avlNotes = [ 2000, 500, 200, 100, 50, 20, 10, 5, 1];
 checkbtn.addEventListener("click", function validbill() {
-    console.log("click");
     msg.style.display = "none";
-    if(typeof billA.value || typeof cashGiven.value === 'string' )
+
+    if(isNaN(billA.value) || isNaN(cashGiven.value))
     {
         msgshow("please enter number");
     }
 
+
     else{
 
         if(billA.value > 0) {
-            if(cashGiven.value >= billA.value)
+            console.log(cashGiven.value >= billA.value)
+
+
+            if(cashGiven.value <= billA.value)
             {
                 const amntbertrn = cashGiven.value - billA.value;
                 calculateChange(amntbertrn);
@@ -49,6 +53,7 @@ checkbtn.addEventListener("click", function validbill() {
     }
 
 }
+
 
 function msgshow(message)
 {
